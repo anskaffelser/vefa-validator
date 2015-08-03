d135468ef9dc6df32830626dc566e7164b20a845
Erlend Klakegg Bergheim <erlend@klakegg.net>
2015-08-03 22:36:07 +0200

    Refactoring validator-repo.

--- validator-repo/src/main/java/no/difi/vefa/validator/Repo.java
+++ validator-repo/src/main/java/no/difi/vefa/validator/Repo.java
@@ -17,81 +17,138 @@ import java.util.HashSet;
 import java.util.Set;
 import java.util.TreeSet;
 
+/**
+ * This class is a Cli for generation of artifacts.xml in a repo of validation artifacts.
+ */
 public class Repo {
 
+    /**
+     * Logger
+     */
     private static Logger logger = LoggerFactory.getLogger(Repo.class);
 
-    private static JAXBContext jaxbContext;
+    /**
+     * Unmarshaller (XML => Java)
+     */
+    private static Unmarshaller unmarshaller;
 
+    /**
+     * Marshaller (Java => XML)
+     */
+    private static Marshaller marshaller;
+
+    /**
+     * Load JAXBContext used.
+     */
     static {
         try {
-            jaxbContext = JAXBContext.newInstance(Configurations.class, Artifacts.class);
+            // Context
+            JAXBContext jaxbContext = JAXBContext.newInstance(Configurations.class, Artifacts.class);
+
+            // Unmarshaller
+            unmarshaller = jaxbContext.createUnmarshaller();
+
+            // Marshaller
+            marshaller = jaxbContext.createMarshaller();
+            marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
         } catch (JAXBException e) {
             logger.error(e.getMessage(), e);
         }
     }
 
+    /**
+     * Entry point when using cli.
+     */
     public static void main(String... args) throws Exception {
-        final Unmarshaller unmarshaller = jaxbContext.createUnmarshaller();
-        final Marshaller marshaller = jaxbContext.createMarshaller();
-        marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
-
-        for (String directory : args) {
-            Path workDirectory = Paths.get(directory);
-            SourceInstance sourceInstance = new DirectorySource(workDirectory).createInstance();
-            ValidatorEngine validatorEngine = new ValidatorEngine(sourceInstance);
-
-            Set<String> fileFound = new TreeSet<>();
-            Set<String> unavailable = new TreeSet<>();
-
-            for (DocumentDeclaration declaration : validatorEngine.getDeclarations()) {
-                Configuration configuration = new Configuration(validatorEngine.getConfiguration(declaration));
-                configuration.normalize(validatorEngine);
-
-                for (FileType fileType : configuration.getFile())
-                    fileFound.add(fileType.getPath().split("#")[0]);
-                if (configuration.getStylesheet() != null)
-                    fileFound.add(configuration.getStylesheet().getPath().split("#")[0]);
-
-                for (String notLoaded : configuration.getNotLoaded())
-                    unavailable.add(notLoaded);
-            }
-
-            Artifacts artifacts = new Artifacts();
-
-            for (String file : fileFound) {
-                final ArtifactType artifactType = new ArtifactType();
-                artifactType.setFilename(file.substring(file.lastIndexOf("/") + 1));
-
-                // Find timestamp...
-                final PathMatcher matcher = sourceInstance.getFileSystem().getPathMatcher("glob:**/config*.xml");
-
-                Files.walkFileTree(sourceInstance.getFileSystem().getPath(artifactType.getFilename()), new HashSet<FileVisitOption>(), 3, new SimpleFileVisitor<Path>() {
-                    @Override
-                    public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
-                        if (matcher.matches(file)) {
-                            try {
-                                artifactType.setTimestamp(((Configurations) unmarshaller.unmarshal(Files.newInputStream(file))).getTimestamp());
-                            } catch (JAXBException e) {
-                                logger.warn(e.getMessage(), e);
-                            }
-                            return FileVisitResult.TERMINATE;
-                        }
-                        return FileVisitResult.CONTINUE;
-                    }
-                });
-
-                artifacts.getArtifact().add(artifactType);
-
-                logger.info(String.format("Found: %s", artifactType.getFilename()));
-            }
-
-            marshaller.marshal(artifacts, Files.newOutputStream(workDirectory.resolve("artifacts.xml")));
-
-            for (String notLoaded : unavailable) {
-                logger.warn(String.format("Not found: %s", notLoaded));
-            }
-        }
+        // Loop over input. Normally just one folder, but multiple is supported.
+        for (String directory : args)
+            generateArtifacts(Paths.get(directory), true);
     }
 
+    /**
+     * Generates list of artifacts for a given directory.
+     */
+    public static Artifacts generateArtifacts(Path directory, boolean writeToDisk) throws Exception {
+        // Use a regular validator engine to load all artifacts in repo and calculate current configuration.
+        SourceInstance sourceInstance = new DirectorySource(directory).createInstance();
+        ValidatorEngine validatorEngine = new ValidatorEngine(sourceInstance);
+
+        // Holds files associated with current configuration.
+        final Set<String> fileFound = new TreeSet<>();
+        // Holds configurations referenced but not found.
+        Set<String> unavailable = new TreeSet<>();
+
+        // Run through all valid declarations in the validator engine.
+        for (DocumentDeclaration declaration : validatorEngine.getDeclarations()) {
+            // Fetch configuration
+            Configuration configuration = new Configuration(validatorEngine.getConfiguration(declaration));
+            // Normalize it to detect resources used and referenced.
+            configuration.normalize(validatorEngine);
+
+            // Fetch source file from filename in files used for validation.
+            for (FileType fileType : configuration.getFile())
+                fileFound.add(fileType.getPath().split("#")[0]);
+            // Fetch source file for filename in stylesheet.
+            if (configuration.getStylesheet() != null)
+                fileFound.add(configuration.getStylesheet().getPath().split("#")[0]);
+
+            // Collect referenced configurations not detected in repository.
+            for (String notLoaded : configuration.getNotLoaded())
+                unavailable.add(notLoaded);
+        }
+
+        // Holds the final list of artifacts.
+        final Artifacts artifacts = new Artifacts();
+
+        // Matcher used to find configuration files.
+        final PathMatcher matcher = sourceInstance.getFileSystem().getPathMatcher("glob:**/config*.xml");
+
+        // Loop through files in the virtual filesystem.
+        Files.walkFileTree(sourceInstance.getFileSystem().getPath("/"), new HashSet<FileVisitOption>(), 3, new SimpleFileVisitor<Path>() {
+            @Override
+            public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+                // If file is a configuration file...
+                if (matcher.matches(file)) {
+                    try {
+                        String parentString = file.getParent().toString();
+                        // If inspected configuration file is part of the set of detected files.
+                        if (fileFound.contains(parentString)) {
+                            // Read configuration file.
+                            Configurations configurations = ((Configurations) unmarshaller.unmarshal(Files.newInputStream(file)));
+
+                            // New artifact
+                            ArtifactType artifactType = new ArtifactType();
+                            // Detect filename from recorded name as metadata may not correlate with internal name.
+                            artifactType.setFilename(parentString.substring(parentString.lastIndexOf("/") + 1));
+                            // Fetch timestamp of creation.
+                            artifactType.setTimestamp(configurations.getTimestamp());
+                            // Add artifact to list of artifacts.
+                            artifacts.getArtifact().add(artifactType);
+                        }
+                    } catch (JAXBException e) {
+                        // We are only allowed to return IOException.
+                        throw new IOException(e.getMessage(), e);
+                    }
+                }
+                // Next file, please.
+                return FileVisitResult.CONTINUE;
+            }
+        });
+
+        // Set newest (highest) timestamp on artifacts-element.
+        for (ArtifactType artifactType : artifacts.getArtifact())
+            if (artifactType.getTimestamp() > artifacts.getTimestamp())
+                artifacts.setTimestamp(artifactType.getTimestamp());
+
+        // Simply list configurations detected as not loaded by validator.
+        for (String notLoaded : unavailable)
+            logger.warn(String.format("Not found: %s", notLoaded));
+
+        // Save result to disk.
+        if (writeToDisk)
+            marshaller.marshal(artifacts, Files.newOutputStream(directory.resolve("artifacts.xml")));
+
+        // Return list of artifacts.
+        return artifacts;
+    }
 }
