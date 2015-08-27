6aec2e97ec8da1cafab0d46d31e119481c706a66
Erlend Klakegg Bergheim <erlend@klakegg.net>
2015-08-27 10:49:00 +0200

    Adding arguments to sign validation artifacts.

--- validator-build/src/main/java/no/difi/vefa/validator/build/Cli.java
+++ validator-build/src/main/java/no/difi/vefa/validator/build/Cli.java
@@ -1,14 +1,17 @@
 package no.difi.vefa.validator.build;
 
-import org.apache.commons.cli.CommandLine;
-import org.apache.commons.cli.CommandLineParser;
-import org.apache.commons.cli.DefaultParser;
-import org.apache.commons.cli.Options;
+import no.difi.asic.SignatureHelper;
+import org.apache.commons.cli.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import java.io.File;
 import java.nio.file.Paths;
 import java.util.UUID;
 
 public class Cli {
+
+    private static Logger logger = LoggerFactory.getLogger(Cli.class);
 
     public static void main(String... args) throws Exception {
         Options options = new Options();
@@ -17,6 +20,9 @@ public class Cli {
         options.addOption("b", "build", true, "Build identifier");
         options.addOption("n", "name", true, "Name");
         options.addOption("w", "weight", true, "Weight");
+        options.addOption(Option.builder("ksf").desc("Keystore file").hasArg(true).build());
+        options.addOption(Option.builder("ksp").desc("Keystore password").hasArg(true).build());
+        options.addOption(Option.builder("pkp").desc("Private key password").hasArg(true).build());
 
         CommandLineParser parser = new DefaultParser();
         CommandLine cmd = parser.parse(options, args);
@@ -24,11 +30,20 @@ public class Cli {
         for (String arg : cmd.getArgs()) {
             Builder builder = new Builder(Paths.get(arg));
 
+            SignatureHelper signatureHelper = null;
+            if (cmd.hasOption("ksf")) {
+                logger.info("Signing information detected.");
+                signatureHelper = new SignatureHelper(
+                        new File(cmd.getOptionValue("ksf")),
+                        cmd.getOptionValue("ksp"),
+                        cmd.getOptionValue("pkp"));
+            }
+
             builder.build(
                     cmd.getOptionValue("name", "rules"),
                     cmd.getOptionValue("build", UUID.randomUUID().toString()),
                     Long.parseLong(cmd.getOptionValue("weight", "0")),
-                    null
+                    signatureHelper
             );
 
             if (cmd.hasOption("test"))
