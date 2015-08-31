fd5931675a049964cd4de8114d9649e7f7914f08
Erlend Klakegg Bergheim <erlend@klakegg.net>
2015-08-31 13:38:47 +0200

    Implementing deletion of files in sample application inside application.

--- validator-web/src/main/java/no/difi/vefa/validator/service/WorkspaceService.java
+++ validator-web/src/main/java/no/difi/vefa/validator/service/WorkspaceService.java
@@ -4,10 +4,13 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import no.difi.vefa.validator.Validation;
 import no.difi.vefa.validator.api.ValidatorException;
 import no.difi.xsd.vefa.validator._1.Report;
+import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.IOUtils;
+import org.joda.time.DateTime;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
@@ -15,10 +18,7 @@ import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
 import javax.xml.bind.Marshaller;
 import javax.xml.bind.Unmarshaller;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.InputStream;
+import java.io.*;
 import java.util.UUID;
 import java.util.zip.GZIPInputStream;
 import java.util.zip.GZIPOutputStream;
@@ -30,6 +30,8 @@ public class WorkspaceService {
 
     @Value("${workspace}")
     private String dirWorkspace;
+    @Value("${workspace.expire}")
+    private int workspaceExpire;
 
     private Marshaller reportMarshaller;
     private Unmarshaller reportUnmarshaller;
@@ -120,5 +122,26 @@ public class WorkspaceService {
 
     public File getView(String identifier) {
         return new File(getFolder(identifier), "view.html");
+    }
+
+    @Scheduled(fixedDelay = 60 * 60 * 1000, initialDelay = 1000)
+    public void cleanWorkspace() {
+        logger.info("Cleaning workspace.");
+
+        if (workspaceExpire < 0)
+            return;
+
+        for (File f : new File(dirWorkspace).listFiles()) {
+            if (f.isDirectory() && !new File(f, ".keep").exists()) {
+                if (new DateTime(f.lastModified()).isBefore(DateTime.now().minusDays(workspaceExpire))) {
+                    try {
+                        logger.info(String.format("Delete validation '%s'.", f.getName()));
+                        FileUtils.deleteDirectory(f);
+                    } catch (IOException e) {
+                        logger.warn(e.getMessage(), e);
+                    }
+                }
+            }
+        }
     }
 }
