8af45b08708edf85f563efd61f6bcadfae98e848
Erlend Klakegg Bergheim <erlend@klakegg.net>
2016-01-24 02:49:36 +0100

    Adding handling of exception in validator-web.

---
+++ validator-web/src/main/java/no/difi/vefa/validator/controller/ExceptionController.java
@@ -0,0 +1,21 @@
+package no.difi.vefa.validator.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.ModelAndView;
+
+@ControllerAdvice
+@SuppressWarnings("unused")
+public class ExceptionController {
+
+    private static Logger logger = LoggerFactory.getLogger(ExceptionController.class);
+
+    @ExceptionHandler(Exception.class)
+    private ModelAndView handleException(Exception e) {
+        logger.warn(e.getMessage(), e);
+        return new ModelAndView("error", "exception", e);
+    }
+
+}
---
+++ validator-web/src/main/webapp/WEB-INF/views/error.jsp
@@ -0,0 +1,4 @@
+<%@ page language="java" pageEncoding="utf-8" contentType="text/html;charset=utf-8" %>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+
+<div class="alert alert-danger" role="alert" style="font-size: 20pt;"><c:out value="${exception.message}" /></div>
