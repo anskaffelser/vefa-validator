b697b8ed119ee295df574b5b9c1cb40c3ca8c2df
Erlend Klakegg Bergheim <erlend@klakegg.net>
2016-12-09 09:54:24 +0100

    Minor refactoring of XsltRenderer to better support HTML content.

--- validator-core/src/main/java/no/difi/vefa/validator/renderer/XsltRenderer.java
+++ validator-core/src/main/java/no/difi/vefa/validator/renderer/XsltRenderer.java
@@ -1,14 +1,17 @@
 package no.difi.vefa.validator.renderer;
 
 import net.sf.saxon.s9api.*;
-import net.sf.saxon.stax.XMLStreamWriterDestination;
 import no.difi.vefa.validator.api.*;
 import no.difi.vefa.validator.util.PathURIResolver;
 import no.difi.vefa.validator.util.SaxonErrorListener;
 import no.difi.xsd.vefa.validator._1.SettingType;
 import no.difi.xsd.vefa.validator._1.StylesheetType;
 
-import javax.xml.stream.XMLOutputFactory;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
 import javax.xml.transform.stream.StreamSource;
 import java.io.OutputStream;
 import java.nio.file.Files;
@@ -20,7 +23,10 @@ import java.nio.file.Path;
 @RendererInfo({".xsl", ".xslt"})
 public class XsltRenderer implements Renderer {
 
-    private static XMLOutputFactory xmlOutputFactory = XMLOutputFactory.newFactory();
+    // private static XMLOutputFactory xmlOutputFactory = XMLOutputFactory.newFactory();
+
+    private static DocumentBuilderFactory documentBuilderFactory;
+    private static TransformerFactory transformerFactory;
 
     private XsltExecutable xsltExecutable;
 
@@ -30,6 +36,13 @@ public class XsltRenderer implements Renderer {
     private StylesheetType stylesheetType;
 
     private Path path;
+
+    static {
+        documentBuilderFactory = DocumentBuilderFactory.newInstance();
+        // documentBuilderFactory.setNamespaceAware(true);
+
+        transformerFactory = TransformerFactory.newInstance();
+    }
 
     /**
      * {@inheritDoc}
@@ -66,11 +79,17 @@ public class XsltRenderer implements Renderer {
                         new XdmAtomicValue(properties.getString(String.format("stylesheet.%s.%s", stylesheetType.getIdentifier(), setting.getName()), setting.getDefaultValue()))
                 );
 
+            org.w3c.dom.Document doc = documentBuilderFactory.newDocumentBuilder().newDocument();
+
             // Use transformer to write the result to stream.
             xsltTransformer.setSource(new StreamSource(document.getInputStream()));
-            xsltTransformer.setDestination(new XMLStreamWriterDestination(xmlOutputFactory.createXMLStreamWriter(outputStream, "UTF-8")));
+            // xsltTransformer.setDestination(new XMLStreamWriterDestination(xmlOutputFactory.createXMLStreamWriter(outputStream, "UTF-8")));
+            xsltTransformer.setDestination(new DOMDestination(doc));
             xsltTransformer.transform();
             xsltTransformer.close();
+
+            Transformer transformer = transformerFactory.newTransformer();
+            transformer.transform(new DOMSource(doc), new StreamResult(outputStream));
         } catch (Exception e) {
             throw new ValidatorException("Unable to render document.", e);
         }
