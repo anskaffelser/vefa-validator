9f57df974015b3e5481663867e90422c7552a56d
Erlend Klakegg Bergheim <erlend@klakegg.net>
2018-12-19 10:23:58 +0100

    Fixing minor bugs.

--- validator-core/src/main/java/no/difi/vefa/validator/util/XmlUtils.java
+++ validator-core/src/main/java/no/difi/vefa/validator/util/XmlUtils.java
@@ -5,21 +5,24 @@ import java.util.regex.Pattern;
 
 public class XmlUtils {
 
-    private static final Pattern rootTagPattern = Pattern.compile(
+    private static final Pattern ROOT_TAG_PATTERN = Pattern.compile(
             "<(?!http[s]{0,1}://)(\\w*:{0,1}[^<?|^<!^]*?)>", Pattern.MULTILINE);
 
-    private static final Pattern namespacePattern = Pattern.compile(
+    private static final Pattern NAMESPACE_PATTERN = Pattern.compile(
             "xmlns:{0,1}([A-Za-z0-9\\-]*)\\w*=\\w*[\"']{1}(.+?)[\"']{1}", Pattern.MULTILINE);
 
+    private static final Pattern COMMENTS_PATTERN =
+            Pattern.compile("<!--(.+?)-->", Pattern.MULTILINE);
+
     public static String extractRootNamespace(String xmlContent) {
-        Matcher matcher = rootTagPattern.matcher(removeComments(xmlContent));
+        Matcher matcher = ROOT_TAG_PATTERN.matcher(removeComments(xmlContent));
         if (matcher.find()) {
             String rootElement = matcher.group(1).trim().replace("\n", " ").replace("\r", "").replace("\t", " ");
             // logger.debug("Root element: {}", rootElement);
             String rootNs = rootElement.split(" ", 2)[0].contains(":") ? rootElement.substring(0, rootElement.indexOf(":")) : "";
             // logger.debug("Root ns: {}", rootNs);
 
-            Matcher nsMatcher = namespacePattern.matcher(rootElement);
+            Matcher nsMatcher = NAMESPACE_PATTERN.matcher(rootElement);
             while (nsMatcher.find()) {
                 // logger.debug(nsMatcher.group(0));
 
@@ -33,7 +36,7 @@ public class XmlUtils {
     }
 
     public static String extractLocalName(String xmlContent) {
-        Matcher matcher = rootTagPattern.matcher(removeComments(xmlContent));
+        Matcher matcher = ROOT_TAG_PATTERN.matcher(removeComments(xmlContent));
         if (matcher.find()) {
             String rootElement = matcher.group(1).trim().replace("\n", " ").replace("\r", "").replace("\t", " ");
             // logger.debug("Root element: {}", rootElement);
@@ -45,11 +48,7 @@ public class XmlUtils {
     }
 
     public static String removeComments(String xmlContent) {
-        while (xmlContent.contains("<!--"))
-            xmlContent = xmlContent.substring(0,
-                    xmlContent.indexOf("<!--")) + xmlContent.substring(xmlContent.indexOf("-->") + 3);
-
-        return xmlContent;
+        return COMMENTS_PATTERN.matcher(xmlContent).replaceAll("");
     }
 
     XmlUtils() {
